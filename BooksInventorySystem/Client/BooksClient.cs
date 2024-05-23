d84a209f5547cdad7e9a7bd4db6ac38c67ecfe94
cliff-codes <simplecodes2580@gmail.com>
2024-05-23 15:48:18 +0100

    Home page of the catalog system implemented

---
+++ BooksInventorySystem/Client/BooksClient.cs
@@ -0,0 +1,70 @@
+﻿namespace BooksInventorySystem;
+
+public class BooksClient
+{
+    // private readonly
+
+    private readonly List<Book> books = [
+        new(){
+            Id = 1,
+            Title = "Pride and Prejudice",
+            Authors = "Jane Austen",
+            PublishedDate = new DateOnly(1813, 01, 23),
+            Genre = "Classic Literature",
+            Price = 3.99M 
+        },
+        new(){
+            Id = 2,
+            Title = "The Lord of the Rings: The Fellowship of the Ring",
+            Authors = "J.R.R. Tolkien",
+            PublishedDate = new DateOnly(1954, 11, 13),
+            Genre = "Fantasy",
+            Price = 12.99M 
+        },
+        new(){
+            Id = 3,
+            Title = "To Kill a Mockingbird",
+            Authors = "Harper Lee",
+            PublishedDate = new DateOnly(1960, 04, 12),
+            Genre = "Historical Fiction",
+            Price = 13.99M 
+        },
+        new(){
+            Id = 4,
+            Title = "The Hitchhiker's Guide to the Galaxy",
+            Authors = "Douglas Adams",
+            PublishedDate = new DateOnly(1979, 01, 23),
+            Genre = "Science Fiction",
+            Price = 9.99M 
+        },
+        new(){
+            Id = 5,
+            Title = "One Hundred Years of Solitude",
+            Authors = "Gabriel García Márquezn",
+            PublishedDate = new DateOnly(1967, 12, 03),
+            Genre = "Magical Realism",
+            Price = 11.99M 
+        },
+        new(){
+            Id = 6,
+            Title = "Frankenstein",
+            Authors = "Mary Shelley",
+            PublishedDate = new DateOnly(1818, 01, 23),
+            Genre = "Gothic Fiction",
+            Price = 6.99M 
+        },
+        new(){
+            Id = 7,
+            Title = "Dune",
+            Authors = "Frank Herbert",
+            PublishedDate = new DateOnly(1813, 01, 23),
+            Genre = "Science Fiction",
+            Price = 14.99M 
+        },
+    ];
+
+    //providing access to books 
+    public List<Book> GetBooks() => books;
+
+
+}
---
+++ BooksInventorySystem/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BooksInventorySystem;
+
+public class Book
+{
+    public int Id {get; set;}
+    public required string Title {get; set;}
+    public required string Authors {get; set;}
+    public decimal Price {get; set;}
+    public required string Genre {get; set;}
+    public required DateOnly PublishedDate {get; set;}
+}
